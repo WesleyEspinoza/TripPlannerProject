5742c77bda8b1003f20b08516824a0c99d20a1eb
Erick Wesley Espinoza <Wesleyespinoza1021@gmail.com>
2019-05-09 19:32:44 -0700

    re initial commit

--- TripPlannerProject/AppDelegate.swift
+++ TripPlannerProject/AppDelegate.swift
@@ -24,8 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         navigationController.navigationBar.isTranslucent = true
         self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
-        GMSServices.provideAPIKey("AIzaSyAjxrLD-yF8g7bPGvdGm0Eu7X-pWCGYsIw")
-        GMSPlacesClient.provideAPIKey("AIzaSyAjxrLD-yF8g7bPGvdGm0Eu7X-pWCGYsIw")
+        GMSServices.provideAPIKey("")
+        GMSPlacesClient.provideAPIKey("")
         return true
     }
 
